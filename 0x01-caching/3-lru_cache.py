e125d95dd978da4fa24b850f89dab6884698df47
ndiritu-prisca <priscandiritu@outlook.com>
2023-10-26 05:25:10 +0300

    LRU

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" LRUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """Initializes parent class"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Assigns to the dictionary self.cache_data the item value for the key
+        key.
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                removed_key = self.queue.pop(0)
+                self.cache_data.pop(removed_key)
+                print(f"DISCARD: {removed_key}")
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """
+        Returns the value in self.cache_data linked to key otherwise, None
+        """
+        if key is not None:
+            if key in self.cache_data:
+                self.queue.remove(key)
+                self.queue.append(key)
+                return self.cache_data[key]
+        return None
