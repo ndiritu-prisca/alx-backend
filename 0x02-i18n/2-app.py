448989057b6fd6198fae4799fd240c26b33c62c0
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-01 02:54:22 +0300

    Pycodestyle check

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,13 +22,14 @@ app.config.from_object(Config)
 @app.route('/', methods=["GET"], strict_slashes=False)
 def index() -> str:
     """Method to define index route"""
-    return render_template('2-index.html')
+    return render_template('1-index.html')
 
 
 @babel.localeselector
 def get_locale():
     """Method to get language to use"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == '__main__':
     app.run()
