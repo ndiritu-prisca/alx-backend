0ce00f947da9a277569aac9e964f97e80fc0b5a9
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-01 02:12:51 +0300

    Check pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """Method to define index route"""
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
